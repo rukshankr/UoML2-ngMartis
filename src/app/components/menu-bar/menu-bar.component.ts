09ef5066150cfe5174de68b8b87636094a7c643c
rukshankr <rukshankr@users.noreply.github.com>
2020-12-26 09:23:52 +0530

    incomplete menu bar

---
+++ src/app/components/menu-bar/menu-bar.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-menu-bar',
+  templateUrl: './menu-bar.component.html',
+  styleUrls: ['./menu-bar.component.scss'],
+})
+export class MenuBarComponent implements OnInit {
+
+  constructor(private menu: MenuController) { }
+
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  ngOnInit() {
+    this.openFirst();
+  }
+
+}
