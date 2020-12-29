3d23e12566557c58eaf693a4f54e84d869228c1c
Dion Weiman <dion.18@itfac.mrt.ac.lk>
2020-12-29 11:44:28 +0530

    Added the Asset creation and Inspection creation pages

--- src/app/pages/selection/selection.page.ts
+++ src/app/pages/selection/selection.page.ts
@@ -12,62 +12,62 @@ import { AlertController } from '@ionic/angular';
 export class SelectionPage implements OnInit {
 	constructor(public atrCtrl: AlertController) {}
 
-	// async ionViewWillEnter() {
-	// 	let alert = this.atrCtrl.create({
-	// 		message: 'Enter PIN',
-	// 		inputs: [
-	// 			{
-	// 				name: 'pin',
-	// 				placeholder: 'Enter PIN',
-	// 				type: 'password',
-	// 				min: '0000',
-	// 				max: '9999'
-	// 			}
-	// 		],
-	// 		buttons: [
-	// 			{
-	// 				text: 'Forgot password',
-	// 				role: 'cancel',
-	// 				handler: async (data) => {
-	// 					console.log('You forgot password');
-	// 					let alert = this.atrCtrl.create({
-	// 						message: 'Forgot Password?',
-	// 						subHeader: 'Enter the email',
-	// 						inputs: [
-	// 							{
-	// 								name: 'email',
-	// 								placeholder: 'Enter email'
-	// 							}
-	// 						],
-	// 						buttons: [ 'OK' ]
-	// 					});
-	// 					(await alert).present();
-	// 				}
-	// 			},
-	// 			{
-	// 				text: 'Login',
-	// 				handler: async (data) => {
-	// 					if (data.pin == '1234') {
-	// 						console.log('Success');
-	// 						// login is valid
-	// 					} else {
-	// 						console.log('fail');
-	// 						let alert = this.atrCtrl.create({
-	// 							message: 'Invalid',
-	// 							subHeader: 'Enter the correct PIN',
-	// 							buttons: [ 'OK' ]
-	// 						});
-	// 						(await alert).present();
+	async ionViewWillEnter() {
+		let alert = this.atrCtrl.create({
+			message: 'Enter PIN',
+			inputs: [
+				{
+					name: 'pin',
+					placeholder: 'Enter PIN',
+					type: 'password',
+					min: '0000',
+					max: '9999'
+				}
+			],
+			buttons: [
+				{
+					text: 'Forgot password',
+					role: 'cancel',
+					handler: async (data) => {
+						console.log('You forgot password');
+						let alert = this.atrCtrl.create({
+							message: 'Forgot Password?',
+							subHeader: 'Enter the email',
+							inputs: [
+								{
+									name: 'email',
+									placeholder: 'Enter email'
+								}
+							],
+							buttons: [ 'OK' ]
+						});
+						(await alert).present();
+					}
+				},
+				{
+					text: 'Login',
+					handler: async (data) => {
+						if (data.pin == '1234') {
+							console.log('Success');
+							// login is valid
+						} else {
+							console.log('fail');
+							let alert = this.atrCtrl.create({
+								message: 'Invalid',
+								subHeader: 'Enter the correct PIN',
+								buttons: [ 'OK' ]
+							});
+							(await alert).present();
 
-	// 						//this.ionViewWillEnter();
-	// 						return false;
-	// 					}
-	// 				}
-	// 			}
-	// 		]
-	// 	});
-	// 	(await alert).present();
-	// }
+							//this.ionViewWillEnter();
+							return false;
+						}
+					}
+				}
+			]
+		});
+		(await alert).present();
+	}
 
 	async showPromptAlert() {
 		let alert = this.atrCtrl.create({
