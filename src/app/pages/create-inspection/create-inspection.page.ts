e01a9cef4a086c61330e997b9d99f31ef822f119
Dion Weiman <dion.18@itfac.mrt.ac.lk>
2021-01-13 22:16:31 +0530

    Modified the Create Asset page

--- src/app/pages/create-inspection/create-inspection.page.ts
+++ src/app/pages/create-inspection/create-inspection.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
+import { InspectionService } from "src/app/services/create-inspection.service";
+import { Geolocation } from "@ionic-native/geolocation/ngx";
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-create-inspection',
@@ -6,10 +10,68 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-inspection.page.scss'],
 })
 export class CreateInspectionPage implements OnInit {
+  results: object[]; 
 
-  constructor() { }
+  opost = new Posts();
+
+  createInspectionForm = this.formBuilder.group({
+    TestID: [""],
+    AssetID: [""],
+    InspectorID: [""],
+    SupervisorID: [""],
+    Frequency: [""],
+    TestModuleID: [""],
+    Priority: [""]
+  });
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private inspectionService: InspectionService,
+    private geolocation: Geolocation,
+    private alertCtrl: AlertController) { }
 
   ngOnInit() {
   }
 
+  onSave() {
+    this.opost = this.createInspectionForm.value;
+
+    console.log("Page Saved", this.opost);
+
+     this.inspectionService.post(this.opost).subscribe((data) => {
+      console.log("Post method success?: ", data);
+      if(data){
+        this.showAlert(true);
+      }else{
+        this.showAlert(false);
+      }
+    });
+
+
+
+}
+
+async showAlert(val){
+  await this.alertCtrl.create({
+    header: "Result",
+    message: val ? "Test added Sucessfully": "Error",
+    buttons: [{
+      text: "OK",
+      handler: () =>{
+        this.createInspectionForm.reset();
+      }
+    }]
+  }).then(res => res.present())
+}
+
+}
+
+export class Posts {
+  TestID: string;
+  AssetID: string;
+  InspectorID: string;
+  SupervisorID: string;
+  Frequency: string;
+  TestModuleID: string;
+  Priority: string;
 }
---
+++ src/app/services/create-inspection.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Posts } from '../pages/create-inspection/create-inspection.page';
+ 
+ 
+@Injectable({
+  providedIn: 'root'
+})
+export class InspectionService {
+  /**
+   * Constructor of the Service with Dependency Injection
+   * @param http The standard Angular HttpClient to make requests
+   */
+  constructor(private http: HttpClient) { }
+
+////////////////if this doesn't work, it's not an error here, but in the API//////////////////////
+  post(opost: Posts): Observable <any> {
+    return this.http.post("http://localhost:3000/test/createNewTest",opost);
+  }
+}
+
