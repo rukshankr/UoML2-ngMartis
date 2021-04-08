5d0135ba6b0357173bfc6551b6b4f420e0efcb88
yomalp <yomalp777@gmail.com>
2021-04-08 23:16:35 +0530

    Fixes

--- src/app/services/inspection-list.service.ts
+++ src/app/services/inspection-list.service.ts
@@ -1,48 +1,43 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
-import { HttpClient } from "@angular/common/http";
-import { Geolocation } from "@ionic-native/geolocation/ngx";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Injectable()
 export class inspectionListService {
-  latitude;
-  longitude;
-  constructor(private http: HttpClient, private geolocation: Geolocation) {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.latitude = resp.coords.latitude.toString();
-      this.longitude = resp.coords.longitude.toString();
-    });
-  }
+	latitude;
+	longitude;
+	constructor(private http: HttpClient, private geolocation: Geolocation) {
+		this.geolocation.getCurrentPosition().then((resp) => {
+			this.latitude = resp.coords.latitude.toString();
+			this.longitude = resp.coords.longitude.toString();
+		});
+	}
 
-  getinspections(): Observable<any> {
-    //return this.httpclient.get('https://martisapiversion1.herokuapp.com/test/getTests');
-    return this.http.get(
-      "https://martisapiversion1.herokuapp.com/test/getTests"
-    );
-  }
+	getinspections(): Observable<any> {
+		//return this.httpclient.get('https://martisapiversion1.herokuapp.com/test/getTests');
+		return this.http.get('https://martisapiversion1.herokuapp.com/test/getTests');
+	}
 
-  async getCoords() {
-    this.geolocation.getCurrentPosition().then((resp) => {
-      this.latitude = resp.coords.latitude.toString();
-      this.longitude = resp.coords.longitude.toString();
-    });
-  }
+	async getCoords() {
+		this.geolocation.getCurrentPosition().then((resp) => {
+			this.latitude = resp.coords.latitude.toString();
+			this.longitude = resp.coords.longitude.toString();
+		});
+	}
 
-  sortInspectionsByDistance(): Observable<any> {
-    // console.log(this.longitude);
-    // console.log(this.latitude);
-    this.getCoords();
-    return this.http.post(
-      "https://martisapiversion1.herokuapp.com/test/orderByLocationAndInspector",
-      {
-        empLatitude: this.latitude,
-        empLongitude: this.longitude,
-        empId: "EMP101",
-      }
-    );
-  }
+	sortInspectionsByDistance(): Observable<any> {
+		// console.log(this.longitude);
+		// console.log(this.latitude);
+		this.getCoords();
+		return this.http.post('https://martisapiversion1.herokuapp.com/test/orderByLocationAndInspector', {
+			empLatitude: this.latitude,
+			empLongitude: this.longitude,
+			empId: 'EMP101'
+		});
+	}
 
-  sortInspectionsByPriority(): Observable<any> {
-    return this.http.get("http://localhost:3000/test/orderByPriority");
-  }
+	sortInspectionsByPriority(): Observable<any> {
+		return this.http.get('https://martisapiversion1.herokuapp.com/test/orderByPriority');
+	}
 }
