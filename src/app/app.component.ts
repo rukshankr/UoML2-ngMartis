7e9bdd1225d8386cee8d1c8ae81243813ca6e71e
Dion Weiman <dion.18@itfac.mrt.ac.lk>
2021-05-24 21:53:27 +0530

    Checking if Inspection sorting works 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,8 +27,8 @@ export class AppComponent implements OnInit {
 
   UserID: BehaviorSubject<string> = new BehaviorSubject("");
   UserIDsub = this.UserID.asObservable();
-  UserRole: BehaviorSubject<string> = new BehaviorSubject("");
-  UserRolesub = this.UserRole.asObservable();
+  EmpRole: BehaviorSubject<string> = new BehaviorSubject("Tester");
+  UserRolesub = this.EmpRole.asObservable();
 
   constructor(
     private platform: Platform,
@@ -66,7 +66,7 @@ export class AppComponent implements OnInit {
         .then((data) => {
           console.log(data);
           this.userRole = data.family_name.split(" ")[0];
-          this.UserRole.next(this.userRole);
+          this.EmpRole.next(this.userRole);
           this.EmpId = data.family_name.split(" ")[2];
           this.UserID.next(this.EmpId);
           this.userName = data.given_name;
@@ -96,7 +96,7 @@ export class AppComponent implements OnInit {
           this.deviceAuth.getUserNameAndRole(this.EmpId).subscribe((user) => {
             this.userName = user.data[0].Name;
             this.userRole = user.data[0].Title;
-            this.UserRole.next(this.userRole);
+            this.EmpRole.next(this.userRole);
           });
         });
       })
