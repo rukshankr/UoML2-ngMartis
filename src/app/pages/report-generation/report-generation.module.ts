0bf4e6a219f42089714d9a658adefc64339e2519
yomalp <yomalp777@gmail.com>
2021-05-25 08:26:15 +0530

    Fixed the pdf generator

--- src/app/pages/report-generation/report-generation.module.ts
+++ src/app/pages/report-generation/report-generation.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { DatePipe } from '@angular/common';
-
 import { IonicModule } from '@ionic/angular';
-
 import { ReportGenerationPageRoutingModule } from './report-generation-routing.module';
-
 import { ReportGenerationPage } from './report-generation.page';
-
 import { CreateReportEmpidService } from 'src/app/services/create-report-empid.service';
 
 @NgModule({
--- src/app/report-generation-asset/report-generation-asset.page.ts
+++ src/app/report-generation-asset/report-generation-asset.page.ts
@@ -5,6 +5,9 @@ import { AlertController, Platform } from '@ionic/angular';
 import { CreateReportEmpidService } from 'src/app/services/create-report-empid.service';
 import { DatePipe } from '@angular/common';
 import { SqliteService } from 'src/app/services/sqlite.service';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
 	selector: 'app-report-generation-asset',
@@ -15,6 +18,10 @@ export class ReportGenerationAssetPage implements OnInit {
 	opost = new Aposts();
 	desktop: boolean = true;
 	log: string = '';
+	pdfObj = null;
+	photoPreview = null;
+	logoData = null;
+
 	createReportForm = this.formBuilder.group({
 		assetID: [
 			'',
@@ -57,6 +64,7 @@ export class ReportGenerationAssetPage implements OnInit {
 					this.showAlert(false);
 				}
 			});
+			this.loadLocalAssetToBase64();
 		} else {
 			try {
 				//connect
@@ -127,6 +135,153 @@ export class ReportGenerationAssetPage implements OnInit {
 				this.assets = Array.of(data.data);
 			});
 		}
+	}
+
+	loadLocalAssetToBase64() {
+		this.http.get('./assets/1200px-Wabtec_Logo.svg.png', { responseType: 'blob' }).subscribe((res) => {
+			const reader = new FileReader();
+			reader.onloadend = () => {
+				this.logoData = reader.result;
+			};
+			reader.readAsDataURL(res);
+		});
+	}
+
+	downloadPdf() {
+		let logo = { image: this.logoData, width: 100 };
+
+		const docDefinition = {
+			watermark: { text: 'M.A.R.T.I.S', color: 'blue', opacity: 0.1, bold: true },
+			content: [
+				{
+					columns: [
+						logo,
+						{
+							text: new Date(),
+							alignemnet: 'right'
+						}
+					]
+				},
+				{ text: 'Report', style: 'header' },
+				{
+					columns: [
+						{
+							width: '50%',
+							text: 'Inspector Details',
+							style: 'subheader'
+						},
+
+						{
+							width: '50%',
+							text: '        '
+						}
+					]
+				},
+				{
+					columns: [
+						{
+							width: '50%',
+							text: 'Employee Name :'
+						},
+						{
+							width: '50%',
+							text: this.lst[0][0].Name
+						}
+					]
+				},
+				{
+					columns: [
+						{
+							width: '50%',
+							text: 'Region :'
+						},
+						{
+							width: '50%',
+							text: this.lst[0][0].Region
+						}
+					]
+				},
+				{
+					columns: [
+						{
+							width: '50%',
+							text: 'Email :'
+						},
+						{
+							width: '50%',
+							text: this.lst[0][0].Email
+						}
+					]
+				},
+				{ text: '----------------------------------------------------------------------', style: 'header' },
+				{
+					columns: [
+						{
+							width: '50%',
+							text: 'Inspection Details',
+							style: 'subheader'
+						},
+
+						{
+							width: '50%',
+							text: '       '
+						}
+					]
+				},
+				{
+					columns: [
+						{
+							width: '25%',
+							text: 'Test No '
+						},
+						{
+							width: '45%',
+							text: 'Test describtion'
+						},
+						{
+							width: '25%',
+							text: 'Status'
+						}
+					]
+				},
+				this.lst[0].map(function(item) {
+					return {
+						columns: [
+							{
+								width: '25%',
+
+								text: item.TestID
+							},
+							{
+								width: '45%',
+								text: item.comments
+							},
+							{
+								width: '25%',
+								text: item.Result
+							}
+						]
+					};
+				})
+			],
+			styles: {
+				header: {
+					fontSize: 18,
+					bold: true,
+					margin: [ 0, 15, 0, 0 ]
+				},
+				subheader: {
+					fontSize: 14,
+					bold: true,
+					margin: [ 0, 15, 0, 0 ]
+				}
+			}
+		};
+
+		this.pdfObj = pdfMake.createPdf(docDefinition);
+		console.log(this.pdfObj);
+
+		this.pdfObj.download();
 	}
 
 	async getEmpsAssets() {
